e54eb329c5f0d483f782cafc6b0d908a27d33208
nick <aitgroup.nick@gmail.com>
2018-04-04 00:22:59 +0800

    add for underscore example to used

--- routes/nick.js
+++ routes/nick.js
@@ -1,10 +1,10 @@
 var express = require('express');
+var _ = require('underscore');
 var router = express.Router();
-
+var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond nick!!');
-  console.log("12312313");
+  res.send(evens);
 });
 
 module.exports = router;
